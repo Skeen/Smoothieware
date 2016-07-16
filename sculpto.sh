21e879053d88382549a27c1c5b9777ffe7758310
Emil 'Skeen' Madsen <sovende@gmail.com>
2016-07-16 17:01:55 +0200

    Use key on sculpto.sh

--- sculpto.sh
+++ sculpto.sh
@@ -3,5 +3,5 @@
 cp ConfigSamples/Sculptoboard/config src/config.default
 ./BuildShell make -j8
 make combined
-scp LPC1768/main-combined.hex root@$1:
-ssh root@$1 "/root/PrinterAPI/tools/lpc_flasher/flash.sh /root/main-combined.hex"
+scp -i ~/.ssh/auto LPC1768/main-combined.hex root@$1:
+ssh -i ~/.ssh/auto root@$1 "/root/PrinterAPI/tools/lpc_flasher/flash.sh /root/main-combined.hex"
