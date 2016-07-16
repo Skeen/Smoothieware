e363c3aaa22dd2a918bb602fb6b3cb33ca9a17db
Emil 'Skeen' Madsen <sovende@gmail.com>
2016-07-16 15:37:59 +0200

    Fixed sculpto transfer ship

--- sculpto.sh
+++ sculpto.sh
@@ -3,4 +3,5 @@
 cp ConfigSamples/Sculptoboard/config src/config.default
 ./BuildShell make -j8
 make combined
-scp LPC1768/main-combined.hex root@192.168.1.111:
+scp LPC1768/main-combined.hex root@$1:
+ssh root@$1 "/root/PrinterAPI/tools/lpc_flasher/flash.sh /root/main-combined.hex"
